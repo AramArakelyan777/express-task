0d9ae2027d289e7860085259b8c32f844aeb9898
Aram Arakelyan <aram777arakelyan@gmail.com>
2025-04-02 15:37:30 +0400

    visit count tracked

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 import dotenv from "dotenv"
 import express from "express"
+import session from "express-session"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
 const app = express()
 
-const PORT = process.env.SERVER_PORT
+const PORT = process.env.SERVER_PORT || 5000
+
+app.use(cookieParser())
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET_KEY,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false, httpOnly: true },
+    })
+)
+
+app.get("/track", (req, res) => {
+    if (!req.session.visits) {
+        req.session.visits = 0
+    }
+    req.session.visits++
+
+    res.cookie("visitCount", req.session.visits, {
+        maxAge: 1000 * 60 * 15,
+    })
+
+    res.set("X-Visit-Count", req.session.visits)
+
+    res.json({
+        message: "Visit count updated.",
+        visitCount: req.session.visits,
+    })
+})
 
 const start = () => {
     try {
@@ -14,6 +44,7 @@ const start = () => {
         })
     } catch (error) {
         console.log(error)
+        process.exit(1)
     }
 }
 
